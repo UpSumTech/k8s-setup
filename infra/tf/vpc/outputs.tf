fd8ef33cedd0d8381f9c8db2c955dd802c88f9fb
Suman Mukherjee <sumanmukherjee03@gmail.com>
2019-07-10 21:15:36 +0530

    Updated the terraform output for the vpc

--- infra/tf/vpc/outputs.tf
+++ infra/tf/vpc/outputs.tf
@@ -1,5 +1,21 @@
+output "region" {
+  value = ["${local.region}"]
+}
+
+output "availability_zones" {
+  value = ["${module.k8s_vpc.azs}"]
+}
+
+output "k8s_vpc_name" {
+  value = "k8s_vpc"
+}
+
 output "k8s_vpc_id" {
   value = "${module.k8s_vpc.vpc_id}"
+}
+
+output "k8s_vpc_cidr" {
+  value = "${module.k8s_vpc.vpc_cidr_block}"
 }
 
 output "k8s_vpc_private_subnets" {
@@ -18,6 +34,22 @@ output "k8s_vpc_public_subnet_cidrs" {
   value = "${module.k8s_vpc.public_subnets_cidr_blocks}"
 }
 
+output "k8s_vpc_private_route_table_ids" {
+  value = ["${module.k8s_vpc.private_route_table_ids}"]
+}
+
+output "k8s_vpc_public_route_table_ids" {
+  value = ["${module.k8s_vpc.public_route_table_ids}"]
+}
+
+output "k8s_nat_public_ips" {
+  value = "${module.k8s_vpc.nat_public_ips}"
+}
+
+output "k8s_nat_gateway_ids" {
+  value = "${module.k8s_vpc.natgw_ids}"
+}
+
 output "k8s_sg_http_ingress_id" {
   value = "${aws_security_group.http_ingress.id}"
 }
@@ -26,6 +58,6 @@ output "k8s_sg_https_ingress_id" {
   value = "${aws_security_group.https_ingress.id}"
 }
 
-output "k8s_nat_public_ips" {
-  value = "${module.k8s_vpc.nat_public_ips}"
+output "kops_state_s3_bucket" {
+  value = "${aws_s3_bucket.kops_state.bucket}"
 }
